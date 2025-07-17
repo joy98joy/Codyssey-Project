acaa92a8becaa27dda3106fa99c01a3f34e41882
joy98joy <129839798+joy98joy@users.noreply.github.com>
2025-07-17 19:44:59 +0900

    Update minmax_calculator.py

--- 1- 4 문제/minmax_calculator.py
+++ 1- 4 문제/minmax_calculator.py
@@ -1,15 +1,19 @@
 def main():
     num = input("숫자들을 공백으로 구분하여 입력하세요 (예: 3 9 1 4 2): ")
-    num = num.split(" ")    
+    num = num.split(" ")
+    
+    
     try:
         num = list(map(float, num))
-
+        
          
     except ValueError:
         return print("Invalid input.")
-    num.sort()
-    
+    if num[0] == "":
+        return print("Invalid input.")
+    num.sort()    
     return print(f"Min: {num[0]}, Max: {num[-1]}")
+   
 
         
 
