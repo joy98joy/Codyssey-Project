a885c9336e73beb8580bbcfb7a690b7bc1db84c1
joy98joy <129839798+joy98joy@users.noreply.github.com>
2025-07-24 19:39:15 +0900

    Update priority_calculator.py

--- 1-6/priority_calculator.py
+++ 1-6/priority_calculator.py
@@ -1,13 +1,12 @@
 def main():
   priority_calculator = (input("공백으로 구분하여 입력하세요 예 4 + 5 * 3 - 2: "))
   priority_calculator= priority_calculator.split(" ")
+
   priority_calculator = list(filter(None, priority_calculator))
   if priority_calculator == [] :
         return print("Invalid input.") 
-  OperatorPlus = "+"
-  OperatorMinus= "-"
-
   
+  #312
  
 
 #   match priority_calculator:
@@ -19,6 +18,8 @@ def main():
   # if priority_calculator.sort():
     
   Operator =  ['+', '-', '*', '/']
+
+  
   parsed_tokens = []
   try:
         for token in priority_calculator:
@@ -27,12 +28,18 @@ def main():
             else:
                 # 숫자로 변환 시도 (float)
                 parsed_tokens.append(float(token))
-        
-    # while priority_calculator[num]:
-    
+        if len(parsed_tokens) < 3 :
+            return print("Invalid input.")
+        for i in range(len(parsed_tokens) - 1):
+            if   parsed_tokens[i] in Operator and parsed_tokens[i+1] == float  :
+                return print("Invalid input.")
+            
+            if  parsed_tokens[i] == "-" and parsed_tokens[i+1] == "-"  and  parsed_tokens[i+2] == "-" :
+                parsed_tokens[i:i+2] = '+'                
+  
   except ValueError:
     return print("Invalid input.")
- 
+  
   def add(NumFirst, NumSecond):
     return NumFirst + NumSecond
   def subtract(NumFirst, NumSecond):
@@ -41,7 +48,7 @@ def main():
     return NumFirst * NumSecond
   def divide(NumFirst, NumSecond):
     if NumSecond == 0:
-        raise ZeroDivisionError(" Error: Division by zero")
+      return print("Error: Division by zero.")
     return NumFirst / NumSecond
   i = 0
   while i < len(parsed_tokens):
@@ -60,12 +67,21 @@ def main():
       i += 1
   final_result = parsed_tokens[0]
   # 남은 덧셈/뺄셈을 순서대로 계산
-  for i in range(1, len(parsed_tokens), 2):
-    operator = parsed_tokens[i]
-    next_number = parsed_tokens[i+1]
-    if operator == '+':
-      final_result = add(final_result, next_number)
-    elif operator == '-':
-      final_result = subtract(final_result, next_number)
+
+  try :
+    for i in range(1, len(parsed_tokens), 2):
+        operator = parsed_tokens[i]
+        next_number = parsed_tokens[i+1]
+        if operator == '+':
+            final_result = add(final_result, next_number)
+        elif operator == '-':
+            final_result = subtract(final_result, next_number)
+  except IndexError:
      
-main()
+        return print("Invalid input.")
+  if final_result == None :
+     return None 
+  return print("Result:",final_result )
+     
+if __name__ == "__main__":
+    main()
